4d8d2899480536672c8a3d4a279766a0b1cc4601
Iris-yuyao <45841262+Iris-yuyao@users.noreply.github.com>
2018-12-16 17:21:43 +0800

    解析影片信息

--- src/edu/csuft/wtao/spider/App.java
+++ src/edu/csuft/wtao/spider/App.java
@@ -1,9 +1,52 @@
 package edu.csuft.wtao.spider;
+
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * 
  * @author yuyao
  *
  */
 public class App {
+	//alt+/
+	public static void main(String[] args) {
+		//目标 URL
+		String url="http://movie.douban.com/top250";
+		//使用jsoup抓取文档
+		try {
+			Document doc=Jsoup.connect(url).get();
+			Elements es= doc.select(".grid_view .item");
+			System.out.println(es.size());
+			
+			//创建一个存储影片列表
+			ArrayList<Film> list=new ArrayList<>();
+			
+			for(Element e :es)
+			{
+				Film f=new Film();
+				//每一部影片
+				Element t=e.select(".title").first();
+				String num= e.select(".star span").last().text();
+				System.out.println(t.text()+","+num);
+//              f.id
+//              f.title
+				list.add(f);
+			}
+			//System.out.println(es.size());
+			//String title=doc.title();
+			//String data=doc.data();
+			//System.out.println(title);
+			//System.out.println(data);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
 
 }
